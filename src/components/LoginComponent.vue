c2b30587e3d0ba2c819bca455a35b3cbb3c763dd
Hedi BA <hedi.bahmed@gmail.com>
2020-03-04 15:17:19 +0000

    [FEATURE] VUE ROUTER

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -21,12 +21,15 @@
             <v-toolbar-title>Login form</v-toolbar-title>
           </v-toolbar>
           <v-card-text>
-            <v-form>
+            <v-form v-model="form.valid" :lazy-validation="lazy" ref="form">
               <v-text-field
                 label="Login"
                 name="login"
                 prepend-icon="person"
                 type="text"
+                required
+                v-model="form.login"
+                :rules="form.loginRules"
               />
 
               <v-text-field
@@ -35,12 +38,15 @@
                 name="password"
                 prepend-icon="lock"
                 type="password"
+                required
+                v-model="form.password"
+                :rules="form.passwordRules"
               />
             </v-form>
           </v-card-text>
           <v-card-actions>
             <v-spacer />
-            <v-btn color="primary">Login</v-btn>
+            <v-btn color="primary" @click="handleLogin">Login</v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
@@ -50,9 +56,24 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    source: String,
+  name: 'LoginComponent',
+  data: () => ({
+    form: {
+      valid: false,
+      errors: [],
+      login: null,
+      loginRules: [(v) => !!v || 'Name is required'],
+      password: null,
+      passwordRules: [(v) => !!v || 'Password is required'],
+    },
+  }),
+  methods: {
+    handleLogin() {
+      if (this.$refs.form.validate()) {
+        localStorage.setItem('isAuthenticated', 'true');
+        this.$router.push({ name: 'Home' });
+      }
+    },
   },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,16 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+    beforeEnter(to, from, next) {
+      const isAuthenticated = localStorage.getItem('isAuthenticated');
+      if (!isAuthenticated) next('/login');
+      else next();
+    },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
   {
     path: '/about',
